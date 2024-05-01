1ef3c5c26c59e8218df26895fb9a50805509ff87
karan sharma <59433517+karan1304@users.noreply.github.com>
2024-05-01 23:13:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const uploadMiddleware = multer({ dest: 'uploads/' })
 const PORT = 8000;
 
 mongoose.connect('mongodb+srv://ksharma1342000:raKTm4WLujOKNdmb@cluster0.hk3vxcu.mongodb.net/')
-app.use(cors({credentials:true , origin:'http://localhost:5173'}));
+app.use(cors({credentials:true , origin:['http://localhost:5173',"https://mern-blog-app.onrender.com"]}));
 app.use(express.json());
 app.use(cookieParser());
 app.use('/uploads',express.static(__dirname + '/uploads'));
